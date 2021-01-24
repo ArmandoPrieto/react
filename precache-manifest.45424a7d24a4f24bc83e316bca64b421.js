76bbfe141039168ec9b5180bee8dfa66640c93df
Armando <prieto.arm@gmail.com>
2021-01-24 10:31:23 -0800

    Updates

---
+++ precache-manifest.45424a7d24a4f24bc83e316bca64b421.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02bd20835317998b067a",
+    "url": "/react/static/css/main.4ba51ddc.chunk.css"
+  },
+  {
+    "revision": "02bd20835317998b067a",
+    "url": "/react/static/js/main.02bd2083.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/react/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "fab355eed50d137b0b13cc39549f15ca",
+    "url": "/react/index.html"
+  }
+];
